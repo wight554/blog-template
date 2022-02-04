0c50ab3577c8dc76f0079b2dd02b5c3a2fecf15b
Volodymyr Zhdanov <wight554@gmail.com>
2022-02-04 16:37:03 +0200

    blog-template: inject mongoose

--- server/module/AppModule.ts
+++ server/module/AppModule.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PostModule } from '@server/module/PostModule';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PostModule],
+  imports: [
+    PostModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env'],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
