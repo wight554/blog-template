dffc508ad1aff7debed777fe975038bad8841239
Volodymyr Zhdanov <wight554@gmail.com>
2022-02-19 14:35:59 +0200

    blog-template: remove ts-mockito completely

--- test/server/decorators/UserDecorator.test.ts
+++ test/server/decorators/UserDecorator.test.ts
@@ -1,26 +1,25 @@
 import { ExecutionContext, HttpArgumentsHost } from '@nestjs/common/interfaces';
-import { mock, instance, when } from 'ts-mockito';
 
 import { User } from '@server/decorators/UserDecorator';
 import { getParamDecoratorFactory } from '@test/utils/mocks/getParamDecoratorFactory';
 import { mockUser } from '@test/server/user/mocks/mockUser';
 
-const mockCtx = mock<ExecutionContext>();
-const mockHttpArgumentsHost: HttpArgumentsHost = mock<HttpArgumentsHost>();
-
-when(mockCtx.switchToHttp).thenReturn(() => instance(mockHttpArgumentsHost));
-when(mockHttpArgumentsHost.getRequest).thenReturn(
-  vi.fn().mockReturnValue({
+const mockSwitchToHttp = (): HttpArgumentsHost => ({
+  getRequest: vi.fn().mockReturnValue({
     user: mockUser,
   }),
-);
+  getResponse: vi.fn(),
+  getNext: vi.fn(),
+});
 
-const ctx = instance(mockCtx);
+const mockCtx = <ExecutionContext>{
+  switchToHttp: mockSwitchToHttp,
+};
 
 describe('UserDecorator', function () {
   it('should return user', function () {
     const factory = getParamDecoratorFactory(User);
-    const result = factory(null, ctx);
+    const result = factory(null, mockCtx);
 
     expect(result).toBe(mockUser);
   });
