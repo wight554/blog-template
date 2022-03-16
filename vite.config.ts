9c8eda513cc98e741e28bd154bd41c6921359d1e
Volodymyr Zhdanov <wight554@gmail.com>
2022-03-16 22:25:28 +0200

    blog-template: use cleaner alias implementation

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import preact from '@preact/preset-vite';
 import swc from 'unplugin-swc';
 import checker from 'vite-plugin-checker';
 import path from 'path';
-import hq from 'alias-hq';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const isTest = process.env.NODE_ENV === 'test';
 
@@ -15,18 +15,16 @@ export default defineConfig({
     preact({
       include: '{test/,}src/**/*.{ts,tsx}',
     }),
-    isTest && swc.vite(),
+    tsconfigPaths({ root: __dirname }),
     checker({
       typescript: true,
       eslint: {
         lintCommand: 'eslint "./**/*.{ts,tsx}"',
       },
     }),
+    isTest && swc.vite(),
   ],
   root: path.join(__dirname, 'src'),
-  resolve: {
-    alias: hq.get('rollup'),
-  },
   build: {
     outDir: path.join(__dirname, 'dist/public'),
     emptyOutDir: true,
