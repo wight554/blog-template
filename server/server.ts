5946bd6a2f76b52437548f4457f7eb4e2feacbaa
Volodymyr Zhdanov <wight554@gmail.com>
2022-03-17 09:43:00 +0200

    blog-template: use cleaner alias implementation (#17)

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,3 @@
-const isDev = process.env.NODE_ENV !== 'production';
-
-if (isDev) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  require('alias-hq').get('module-alias');
-}
-
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
@@ -25,7 +18,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
-  if (isDev) {
+  if (process.env.NODE_ENV !== 'production') {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const { getViteServer } = require('@server/get-vite-server');
 
