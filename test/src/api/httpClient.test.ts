ea6fa1d713b87c2689bbd909ff2009001fd1c9a2
Volodymyr Zhdanov <wight554@gmail.com>
2022-03-21 17:39:15 +0200

    blog-template: implement http client (#26)
    * implement http client
    
    * based on fetch api
    
    * improve api for empty responses and add default headers

---
+++ test/src/api/httpClient.test.ts
@@ -0,0 +1,133 @@
+import { httpClient } from '@src/api/httpClient';
+
+const url = 'url';
+const body = { data: 'data' };
+const init = { headers: { Authorization: 'Bearer token' } };
+const response = 'response';
+
+global.fetch = vi.fn().mockImplementation(() => ({ json: vi.fn().mockResolvedValue(response) }));
+
+describe('httpClient', () => {
+  beforeAll(() => {
+    vi.useFakeTimers();
+  });
+
+  afterAll(() => {
+    vi.useRealTimers();
+  });
+
+  describe('delete', () => {
+    it('should make delete request with given init', async () => {
+      await httpClient.delete(url, init);
+
+      expect(fetch).toHaveBeenCalledWith(url, {
+        ...init,
+        headers: { ...init.headers, 'Content-Type': 'application/json; charset=utf-8' },
+        method: 'DELETE',
+      });
+    });
+
+    it('should return response from fetch api', async () => {
+      expect(await httpClient.get(url, init)).toEqual({ data: response });
+    });
+  });
+
+  describe('get', () => {
+    it('should make get request with given init', async () => {
+      await httpClient.get(url, init);
+
+      expect(fetch).toHaveBeenCalledWith(url, {
+        ...init,
+        headers: { ...init.headers, 'Content-Type': 'application/json; charset=utf-8' },
+        method: 'GET',
+      });
+    });
+
+    it('should return response from fetch api', async () => {
+      expect(await httpClient.get(url, init)).toEqual({ data: response });
+    });
+  });
+
+  describe('head', () => {
+    it('should make head request with given init', async () => {
+      await httpClient.head(url, init);
+
+      expect(fetch).toHaveBeenCalledWith(url, {
+        ...init,
+        headers: { ...init.headers, 'Content-Type': 'application/json; charset=utf-8' },
+        method: 'HEAD',
+      });
+    });
+
+    it('should return response from fetch api', async () => {
+      expect(await httpClient.head(url, init)).toEqual({ data: response });
+    });
+  });
+
+  describe('options', () => {
+    it('should make options request with given init', async () => {
+      await httpClient.options(url, init);
+
+      expect(fetch).toHaveBeenCalledWith(url, {
+        ...init,
+        headers: { ...init.headers, 'Content-Type': 'application/json; charset=utf-8' },
+        method: 'OPTIONS',
+      });
+    });
+
+    it('should return response from fetch api', async () => {
+      expect(await httpClient.options(url, init)).toEqual({ data: response });
+    });
+  });
+
+  describe('patch', () => {
+    it('should make patch request with given body and init', async () => {
+      await httpClient.patch(url, body, init);
+
+      expect(fetch).toHaveBeenCalledWith(url, {
+        ...init,
+        headers: { ...init.headers, 'Content-Type': 'application/json; charset=utf-8' },
+        body: JSON.stringify(body),
+        method: 'PATCH',
+      });
+    });
+
+    it('should return response from fetch api', async () => {
+      expect(await httpClient.patch(url, body, init)).toEqual({ data: response });
+    });
+  });
+
+  describe('post', () => {
+    it('should make post request with given body and init', async () => {
+      await httpClient.post(url, body, init);
+
+      expect(fetch).toHaveBeenCalledWith(url, {
+        ...init,
+        headers: { ...init.headers, 'Content-Type': 'application/json; charset=utf-8' },
+        body: JSON.stringify(body),
+        method: 'PATCH',
+      });
+    });
+
+    it('should return response from fetch api', async () => {
+      expect(await httpClient.post(url, body, init)).toEqual({ data: response });
+    });
+  });
+
+  describe('put', () => {
+    it('should make put request with given body and init', async () => {
+      await httpClient.put(url, body, init);
+
+      expect(fetch).toHaveBeenCalledWith(url, {
+        ...init,
+        headers: { ...init.headers, 'Content-Type': 'application/json; charset=utf-8' },
+        body: JSON.stringify(body),
+        method: 'PATCH',
+      });
+    });
+
+    it('should return response from fetch api', async () => {
+      expect(await httpClient.put(url, body, init)).toEqual({ data: response });
+    });
+  });
+});
