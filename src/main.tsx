bee4d00001c789379e33e368f171bb359b30e3d9
Volodymyr Zhdanov <wight554@gmail.com>
2022-03-22 13:20:52 +0200

    blog-template: implement recoil

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { render } from 'preact';
 import { App } from '@src/app';
 import '@src/index.css';
+import { RecoilRoot } from 'recoil';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-render(<App />, document.getElementById('app')!);
+render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>,
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  document.getElementById('app')!,
+);
