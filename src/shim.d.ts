c0898908f3f58b23beaac921e48605a36e2c7299
Volodymyr Zhdanov <wight554@gmail.com>
2022-04-26 22:46:32 +0300

    remove htm shim

--- src/shim.d.ts
+++ src/shim.d.ts
@@ -6,10 +6,6 @@ declare module 'preact/hooks' {
   export * from 'node_modules/preact/hooks/src/index.js';
 }
 
-declare module 'htm/preact' {
-  export * from 'node_modules/htm/preact/index.js';
-}
-
 declare module 'recoil' {
   export * from 'node_modules/recoil/index.js';
 }
