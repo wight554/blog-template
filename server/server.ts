4c7e72000e60067ff9dc29fa85d304cf38dea6c8
Volodymyr Zhdanov <wight554@gmail.com>
2022-04-26 22:51:37 +0300

    import default from fastify-cookie
    * to fix import/no-named-as-default-member

--- server/server.ts
+++ server/server.ts
@@ -4,13 +4,11 @@ import { fileURLToPath } from 'url';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import fastifyCookiePlugin from 'fastify-cookie';
+import { default as fastifyCookie } from 'fastify-cookie';
 import { default as fastifyStatic } from 'fastify-static';
 
 import { AppModule } from '@server/app/AppModule.js';
 import { prettyPrintAddress } from '@server/utils/prettyPrintAddress.js';
-
-const { fastifyCookie } = fastifyCookiePlugin;
 
 const { PORT, HOST } = process.env;
 const port = PORT || 3000;
