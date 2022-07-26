97fd745b6005dab104e9f148c4c56be1e6e39b55
Volodymyr Zhdanov <wight554@gmail.com>
2022-07-26 03:55:25 +0300

    add many hacks

--- vite.config.ts
+++ vite.config.ts
@@ -40,6 +40,56 @@ export default defineConfig({
       exclude: [...coverageExclude, '**/schemas/**'],
     },
     setupFiles: ['test/testSetup.ts', 'test/recoilTestSetup.ts'],
+    deps: {
+      inline: [
+        '@mui/material',
+        '@mui/system',
+        '@mui/private-theming',
+        '@mui/styled-engine',
+        '@mui/utils',
+        '@mui/base',
+        '@emotion/react',
+        '@emotion/styled',
+      ],
+    },
+    alias: [
+      {
+        find: 'recoil',
+        replacement: path.resolve(__dirname, './node_modules/recoil/es/index.mjs'),
+      },
+      {
+        find: 'react-router-dom',
+        replacement: path.resolve(__dirname, './node_modules/react-router-dom/index.js'),
+      },
+      {
+        find: 'react-router',
+        replacement: path.resolve(__dirname, './node_modules/react-router/index.js'),
+      },
+      {
+        find: /@mui\/material\/node/,
+        replacement: '@mui/material',
+      },
+      {
+        find: /@mui\/private-theming\/node/,
+        replacement: '@mui/private-theming',
+      },
+      {
+        find: /@mui\/styled-engine\/node/,
+        replacement: '@mui/styled-engine',
+      },
+      {
+        find: /@mui\/base\/node/,
+        replacement: '@mui/base',
+      },
+      {
+        find: /@mui\/system\/index.js/,
+        replacement: '@mui/system/esm/index.js',
+      },
+      {
+        find: /@mui\/utils\/index.js/,
+        replacement: '@mui/utils/esm/index.js',
+      },
+    ],
   },
   resolve: {
     alias: [
