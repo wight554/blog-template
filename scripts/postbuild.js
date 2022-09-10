e418e4efa140090f2984ff24344121af3dac4315
Volodymyr Zhdanov <wight554@gmail.com>
2022-09-10 17:48:25 +0300

    remove unused imports

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,6 +1,4 @@
 import { readFile, writeFile } from 'fs/promises';
-import { dirname, join, resolve } from 'path';
-import { fileURLToPath } from 'url';
 
 const { imports } = JSON.parse(await readFile(new URL('../package.json', import.meta.url)));
 
